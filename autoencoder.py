4ae971e95e21e8f416ecc3c7314e6399e6e0191a
Ishara Upamal <isharaux@gmail.com>
2019-04-09 23:14:04 +0530

    Updating Readme - Adding Gitignore and removing unnecessary.

--- autoencoder.py
+++ autoencoder.py
@@ -1,1 +1,14 @@
 import tensorflow as tf
+from tensorflow.keras import layers
+
+
+
+def build_model():
+    print(tf.__version__)
+    print(tf.keras.__version__)
+
+
+
+if __name__ == '__main__':
+    build_model()
+
