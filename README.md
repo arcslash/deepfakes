f6b74c8b5aa20cf4d6c9bdac1ddd1107bda1c1e5
Ishara Abeykoon <ishara.abeykoon@axiatadigitallabs.com>
2020-06-03 23:53:03 +0530

    face detection added for video feeds

--- README.md
+++ README.md
@@ -13,30 +13,35 @@ Pytorch
 
 ### DeepFakes for Face Change
 - [x] Adding Face Detection
-- [ ] Developing GAN to generate faces
-- [ ] Face / Object Substitution
+- [ ] Adding Face Tracking through Video Frames
+- [ ] Developing GAN to face style transfer
+- [ ] Face Substitution
 - [ ] Training on New data
 - [ ] UI interface to carryout training
 
 ### DeepFakes for Object change
-Todo 
+- [ ] Training Interface for Object Detection
+- [ ] Object Tracking and Detection
+- [ ] Developing GAN to style transfer
+- [ ] Object Substitution
 
 ## How to get started
 
-As the start make a virtual environment I'hv used python3 ( who use python2 anyway?) 
-Make a virtual environment with whatever name you like, I'hv used env for simplicity, If you don't care you can directly install in the system as well.
+As the start make a virtual environment i've used python3 ( who use python2 anyway?) 
+Make a virtual environment with whatever name you like, i've used env for simplicity, If you don't care you can directly install in the system as well.
+Then Install the requirements as specified in requirements.txt
 
 on linux or mac os
 ```
 python3 -m venv env
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 ```
 
 on windows
 
 ```
 py -m venv env
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 ```
 I am running on Linux so could'nt test on other platforms, but it should work on those platforms as well (If not make a issue? or help me fix it?)
 ## Credits
--- facedetect.py
+++ facedetect.py
@@ -1,27 +1,54 @@
 import torch
 from facenet_pytorch import MTCNN, InceptionResnetV1
+import cv2, mmcv
 from PIL import Image, ImageDraw
+import numpy as np
 
-
-test_image_path = 'data/multi_face1.jpg'
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-mtcnn = MTCNN(keep_all=True, device=device)
-resnet = InceptionResnetV1(pretrained='vggface2').eval()
-img = Image.open(test_image_path)
-
-boxes, _ = mtcnn.detect(img)
-frame_draw = img.copy()
-draw = ImageDraw.Draw(frame_draw)
-for box in boxes:
-    draw.rectangle(box.tolist(), outline=(255, 0, 0), width=6)
-frame_draw.show()
-print(boxes)
 
 class FaceDetector:
-    def __init__(self):
-        super().__init__()
-    def process_video():
-        print("I Process Videos bois")
-    def process_frame():
-        print("I Process frame bois")
+    def __init__(self, input_path, input_type='image'):
+       self.path = input_path
+       self.type = input_type
+       self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+       self.mtcnn = MTCNN(keep_all=True, device=self.device)
+       self.resnet = InceptionResnetV1(pretrained='vggface2').eval()
+    def process(self):
+        if self.type == 'image':
+            print('[+] Processing Image')
+            img = Image.open(self.path)
+            boxes, _ = self.mtcnn.detect(img)
+            frame_draw = img.copy()
+            draw = ImageDraw.Draw(frame_draw)
+            for box in boxes:
+                draw.rectangle(box.tolist(), outline=(255, 0, 0), width=6)
+            frame_draw.save('output.jpg')
+        elif self.type == 'video':
+            print('[+] Processing Video')
+            frames_tracked = []
+            video = mmcv.VideoReader(self.path)
+            frames = [Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)) for frame in video]
+
+            for i, frame in enumerate(frames):
+                print('\rTracking frame: {}'.format(i + 1), end='')
+                boxes, _ = self.mtcnn.detect(frame)
+                
+                frame_draw = frame.copy()
+                draw = ImageDraw.Draw(frame_draw)
+                if type(boxes) != type(None):
+                    for box in boxes:
+                        draw.rectangle(box.tolist(), outline=(255, 0, 0), width=6)
+                frames_tracked.append(frame_draw.resize((640, 360), Image.BILINEAR))
+            dim = frames_tracked[0].size
+            fourcc = cv2.VideoWriter_fourcc(*'FMP4')    
+            video_tracked = cv2.VideoWriter('video_tracked.mp4', fourcc, 25.0, dim)
+            for frame in frames_tracked:
+                video_tracked.write(cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2BGR))
+            video_tracked.release()
+        else:
+            print('Error: Invalid Input Type')
+
+if __name__ == '__main__':
+    # facedetect = FaceDetector('data/single_face1.jpg')
+    # facedetect.process()
+    facedetect = FaceDetector('data/video_test.mp4', 'video')
+    facedetect.process()
